5385a1a089f051b635ee92dfdebaa79e69f9e499
YarkoL <yarko.dev@gmail.com>
2017-09-19 13:14:40 +0300

    appframe in app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
 "use strict"
+import React from 'react';  
+import ReactDOM from 'react-dom';
+
+var appFrame = (
+	<div>
+	<div id="container">
+		<header></header>
+		<main>
+		  <div className="left"></div>
+		  <div className="middle"></div>
+		  <div className="right"></div>
+		</main>
+	</div>
+	</div>
+	);
+
+const destination = document.getElementById("app");
+
+ReactDOM.render( appFrame, destination );
