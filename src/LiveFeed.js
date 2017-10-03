bba28982e06c405ea70c751f0cd1fc9f702aa8d2
YarkoL <yarko.dev@gmail.com>
2017-10-03 13:25:29 +0300

    more handling of socket events

--- src/LiveFeed.js
+++ src/LiveFeed.js
@@ -3,7 +3,8 @@ import React from 'react';
 import Ticket from './Ticket';
 
 let data = []
-const url = 'http://localhost:3030';
+const url = 'http://remotesupport-dev.azurewebsites.net';
+//const url = 'http://localhost:3030';
 
 const TicketList = (props) => { 
   return (
@@ -33,6 +34,19 @@ class LiveFeed extends React.Component {
 	
 	initSocket = () => {
 	    const socket = openSocket(url);
+	    socket.on('connect', () => {
+		    console.log('connected');
+		  });
+		  socket.on('disconnect', () => {
+		    console.log('disconnected');
+		  });
+		  socket.on('error', (err) => {
+		    if(err === 'handshake error') {
+		      console.log('handshake error', err);
+		    } else {
+		      console.log('io error', err);
+		    }
+		});
 	    socket.on('roomname', (data) => {
 	        console.log('socket message '+ JSON.stringify(data));       
 	        this.createTicket(
